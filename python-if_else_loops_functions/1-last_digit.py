bb5ecb23bf042341a5701121ba6ceb587906799a
root <luigino.loayza@gmail.com>
2023-11-22 15:07:07 -0800

    correcion del codigo1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 #abs es para para generar numeros absolutos 
-last_digit = abs(number % 10)
-
-if number > 0:
+last_digit = abs(number) % 10
+if number >= 0:
     print(f"Last digit of {number} is {last_digit}", end=" ")
 else:
-    print(f"Last digit of {number} is -{abs(last_digit)}", end=" ")
+    print(f"Last digit of {number} is -{last_digit}", end=" ")
 
 if last_digit > 5:
     print("and is greater than 5")
