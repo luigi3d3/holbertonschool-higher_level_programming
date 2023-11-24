54c48e5c37c4dd5a7469d5119dfebd12c0b2cae8
root <luigino.loayza@gmail.com>
2023-11-23 19:38:37 -0800

    correcion del codigo

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,5 +5,3 @@ def uppercase(s):
             print("{}".format(chr(ord(char) - ord('a') + ord('A'))), end="")
         else:
             print(char, end="")
-
-uppercase("Best School, 98 Battery street")
