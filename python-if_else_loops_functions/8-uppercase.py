ba8af99864842a3af5c44b0f7f202d29d948d6b6
root <luigino.loayza@gmail.com>
2023-11-23 18:09:54 -0800

    ejercicio 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for char in s:
+        if 'a' <= char <= 'z':
+            print(chr(ord(char) - ord('a') + ord('A')), end="")
+        else:
+            print(char, end="")
