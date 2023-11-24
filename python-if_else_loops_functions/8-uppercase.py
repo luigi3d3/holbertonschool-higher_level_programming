8f63e36eb276523ee15ef6803e501713cacbc8b6
root <luigino.loayza@gmail.com>
2023-11-23 18:14:00 -0800

    introduzco format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(s):
     for char in s:
         if 'a' <= char <= 'z':
-            print(chr(ord(char) - ord('a') + ord('A')), end="")
+            print("{}".format(chr(ord(char) - ord('a') + ord('A'))), end="")
         else:
             print(char, end="")
