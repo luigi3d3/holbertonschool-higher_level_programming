be66d29a710b6e02e0986d9e015a3f94dad75cc5
root <luigino.loayza@gmail.com>
2023-11-25 07:05:36 -0800

    correcion del ejercicio 9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -4,9 +4,3 @@ def print_last_digit(number):
     last_digit = number % 10
     print(last_digit, end="")
     return last_digit
-
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
-
