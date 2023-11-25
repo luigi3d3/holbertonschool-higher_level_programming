5c3e402b0af346d0ea3ac763186383fefe27b946
root <luigino.loayza@gmail.com>
2023-11-25 15:50:26 -0800

    ejercicio2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+
+num_args = len(argv) - 1
+arg_plural = "arguments" if num_args != 1 else "argument"
+end_char = ":" if num_args > 0 else "."
+
+print(f"{num_args} {arg_plural} {end_char}")
+
+for i, arg in enumerate(argv[1:], start=1):
+    print(f"{i}, {arg}")
