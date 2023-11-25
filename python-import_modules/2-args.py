4a51516a5f4e015d92ac9fc9c64d5486f8c59897
root <luigino.loayza@gmail.com>
2023-11-25 15:57:30 -0800

    correcion del ejercicio2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ num_args = len(argv) - 1
 arg_plural = "arguments" if num_args != 1 else "argument"
 end_char = ":" if num_args > 0 else "."
 
-print(f"{num_args} {arg_plural} {end_char}")
+print(f"{num_args} {arg_plural}{end_char}")
 
 for i, arg in enumerate(argv[1:], start=1):
     print(f"{i}: {arg}")
