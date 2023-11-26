e7b3fd254e034707ebb7d122be97a0785e183593
root <luigino.loayza@gmail.com>
2023-11-25 16:52:15 -0800

    ejercicio 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
 
+
 def add_arguments():
 
     total = sum(int(arg) for arg in sys.argv[1:])
 
     print(f"{total}")
 
+
 if __name__ == "__main__":
     add_arguments()
-
