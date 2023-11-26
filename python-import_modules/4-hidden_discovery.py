c0d34e9c5dcbe9fd3a452068e821017fa194d2cf
root <luigino.loayza@gmail.com>
2023-11-26 13:09:50 -0800

    correcion del codigo 3

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,18 @@
-#!/urs/bin/python3
+#!/usr/bin/python3
+import dis
+import marshal
+
+def list_names():
+    with open('hidden_4.pyc', 'rb') as file:
+        code = marshal.load(file)
+
+    instructions = dis.get_instructions(code)
+
+    names = [instruction.argval for instruction in instructions
+             if instruction.opname == 'LOAD_GLOBAL' and not instruction.argval.startswith('__')]
+
+    for name in sorted(set(names)):
+        print(name)
+
+if __name__ == '__main__':
+    list_names()
