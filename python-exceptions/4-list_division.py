343c30760a0955fcedc2b2a4931d6daeb205310e
root <luigino.loayza@gmail.com>
2023-12-27 10:44:57 -0800

    ejercicio4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            division_result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            division_result = 0
+            print("division by 0")
+        except (TypeError, IndexError):
+            division_result = 0
+            print("wrong type")
+        except Exception as e:
+            division_result = 0
+            print(e)
+        finally:
+            result.append(division_result)
+    return result
