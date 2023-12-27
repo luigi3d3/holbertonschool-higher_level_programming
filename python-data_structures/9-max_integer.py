6a6859604e2e02ba77828cb8e7c0117206b2387c
root <luigino.loayza@gmail.com>
2023-12-26 21:24:58 -0800

    ejercicio9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    # Check if the list is empty
+    if not my_list:
+        return None
+
+    max_value = my_list[0]
+
+    for num in my_list:
+        if num > max_value:
+            max_value = numi
