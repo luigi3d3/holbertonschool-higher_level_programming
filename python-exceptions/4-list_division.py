0c6ab17e0b6049c9ad4ccab96a1985c1c84defd4
root <luigino.loayza@gmail.com>
2023-12-27 20:44:20 -0800

    correcion ejercicio4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -8,10 +8,10 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             result.append(0)
-        except (TypeError, IndexError):
+        except (TypeError, ValueError):
             print("wrong type")
             result.append(0)
-        except Exception as e:
+        except IndexError:
             print("out of range")
             result.append(0)
         finally:
