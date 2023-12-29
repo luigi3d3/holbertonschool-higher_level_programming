bece87cfea889db2bd6a25399656e1eb8a5d45ce
root <luigino.loayza@gmail.com>
2023-12-29 08:22:49 -0800

    ejercicio1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size):
-        self.__size = size
 
+"""Square class"""
+
+
+class Square:
+    """
+    Class Square that defines a square.
+
+    Attributes:
+        __size (int): Private instance attribute for the size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a new instance of the Square class.
+    """
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Parameters:
+            size (int): Size of the square.
+        """
+        self.__size = size
