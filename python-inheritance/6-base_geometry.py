94da7a0ffece344c9dc526755b14b2dd19ddb22c
root <luigino.loayza@gmail.com>
2023-12-29 13:52:16 -0800

    ejercicio 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A geometry class with an exception"""
+
+
+class BaseGeometry():
+    """write a method and raise a Exception"""
+
+    def area(self):
+        """Not Implemented"""
+        raise Exception("area() is not implemented")
