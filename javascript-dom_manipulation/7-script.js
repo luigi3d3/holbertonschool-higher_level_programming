5a01be3f1ec48bb93b207c648d0b5b9f6765d509
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-03-14 18:08:58 -0500

    ejercicios

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const listMovies = document.getElementById('list_movies');
+
+    fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+        .then(response => response.json())
+        .then(data => {
+            data.results.forEach(movie => {
+                const listItem = document.createElement('li');
+                listItem.textContent = movie.title;
+                listMovies.appendChild(listItem);
+            });
+        })
+        .catch(error => {
+            console.log('Error fetching data:', error);
+        });
+});
