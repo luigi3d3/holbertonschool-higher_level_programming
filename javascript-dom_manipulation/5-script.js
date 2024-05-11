abca3f01fa48a030f7de291133023beabab3d8a7
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-05-10 19:38:05 -0500

    Delete javascript-dom_manipulation directory

--- javascript-dom_manipulation/5-script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const updateHeaderButton = document.getElementById('update_header');
-    const header = document.querySelector('header');
-
-    updateHeaderButton.addEventListener('click', function () {
-        header.textContent = 'New Header!!!';
-    });
-});
