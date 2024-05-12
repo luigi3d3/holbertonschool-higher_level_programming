545e04afa711796ef12c76b246a52832fa35d768
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-05-11 19:39:36 -0500

    Change the text

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -6,5 +6,5 @@ document.addEventListener('DOMContentLoaded', function () {
         const newItem = document.createElement('li');
         newItem.textContent = 'Item';
         myList.appendChild(newItem);
-    })
-})
+    });
+});
