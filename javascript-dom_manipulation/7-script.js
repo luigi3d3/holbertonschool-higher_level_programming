6e4721da324a3b0e6f157019f0337788530ed664
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-05-19 15:01:41 -0500

    Star Wars movies

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const listFilm = document.getElementById('list-film');
+    const listMovies = document.getElementById('list_movies');
 
     fetch('https://swapi-api.hbtn.io/api/films/?format=json')
         .then(response => response.json())
         .then(data => {
-            data.result.forEach(film => {
-                const listMovies = document.createElement('li');
-                listMovies.textContent = film.title;
-                listFilm.appendChild(listMovies);
+            data.results.forEach(movie => {
+                const listItem = document.createElement('li');
+                listItem.textContent = movie.title;
+                listMovies.appendChild(listItem);
             });
         })
         .catch(error => {
             console.log('Error fetching data:', error);
         });
-})
+});
