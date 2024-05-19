26d20a1a9a40739e42e7e1ef1a907f7fa7608002
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-05-19 13:28:43 -0500

    Star wars character

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const characterDiv = document.getElementById('character');
+
+    fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+        .then
+})
