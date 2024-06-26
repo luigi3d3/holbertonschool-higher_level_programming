6f25bda47e2784d7c5d55cb1a471826811ef56ff
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-06-26 15:11:26 -0500

    getupcode

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,6 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const characterDiv = document.getElementById('character');
+const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
 
-    fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
-        .then
-})
+fetch(url)
+  .then(res => {
+    if (!res.ok) throw new Error(`Sure about that URL? (${res.status})`);
+    return res.json();
+  })
+  .then(data => {
+    const characterElement = document.getElementById('character');
+    let charName = data.name;
+    characterElement.innerHTML = charName;
+  });
