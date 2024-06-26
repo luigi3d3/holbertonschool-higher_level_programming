d9d606c339546e44d2b211089f9a2fc4722baaa0
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-06-26 15:32:49 -0500

    get up code Change the text

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,8 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const updateHeaderButton = document.getElementById('update_header');
-    const header = document.querySelector('header');
+const header = document.querySelector('header');
+const updateHeader = document.getElementById('update_header');
 
-    updateHeaderButton.addEventListener('click', function () {
-        header.textContent = 'New Header!!!';
-    });
-});
+updateHeader.onclick = () => {
+  header.innerHTML = 'New Header!!!';
+};
